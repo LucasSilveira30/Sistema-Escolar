9c6503e49b43f366f7f5e37ebc1775c100fc865c
LucasSilveira30 <silveira30082006@gmail.com>
2026-02-05 05:20:46 -0300

    Create Disciplina.java

---
+++ Disciplina.java
@@ -0,0 +1,11 @@
+public class Disciplina { //relação direcionada com Professor
+    private String descricaoDisciplina;
+
+    public Disciplina(String descricaoDisciplina) {
+        this.descricaoDisciplina = descricaoDisciplina;
+    }
+
+    public String getDescricaoDisciplina() {
+        return descricaoDisciplina;
+    }
+}
